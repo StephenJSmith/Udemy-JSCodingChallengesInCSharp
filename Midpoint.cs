5b989ba177ddebe2f6451619516529b68ddb3675
Stephen Smith <stephensmith_aus@yahoo.com.au>
2019-08-09 11:07:15 +1000

    Midpoint of LinkedList

---
+++ Midpoint.cs
@@ -0,0 +1,24 @@
+﻿namespace CodingChallenge
+{
+    public class Midpoint<T>
+    {
+        public Node<T> Execute(LinkedList<T> list)
+        {
+            var node0 = list.GetFirst();
+            if (node0 == null)
+            {
+                return null;
+            }
+
+            var node1 = list.GetFirst();
+
+            while (node1.Next?.Next != null)
+            {
+                node0 = node0.Next;
+                node1 = node1.Next.Next;
+            }
+
+            return node0;
+        }
+    }
+}
---
+++ MidpointTests.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodingChallenge
+{
+    [TestClass]
+    public class MidpointTests
+    {
+        [TestMethod]
+        public void OddNumberNodesList_ReturnsMiddleNode()
+        {
+            // Arrange
+            var sut = new Midpoint<string>();
+            var list = new LinkedList<string>();
+            var expectedNodeData = "C";
+            list.InsertFirst("A");
+            list.InsertLast("B");
+            list.InsertLast(expectedNodeData);
+            list.InsertLast("D");
+            list.InsertLast("E");
+
+            // Act
+            var actual = sut.Execute(list);
+
+            // Assert
+            Assert.AreEqual(expectedNodeData, actual.Data);
+        }
+        [TestMethod]
+        public void EvenNumberNodesList_ReturnsLastNodeInFirstHalf()
+        {
+            // Arrange
+            var sut = new Midpoint<string>();
+            var list = new LinkedList<string>();
+            var expectedNodeData = "C";
+            list.InsertFirst("A");
+            list.InsertLast("B");
+            list.InsertLast(expectedNodeData);
+            list.InsertLast("D");
+            list.InsertLast("E");
+            list.InsertLast("F");
+
+            // Act
+            var actual = sut.Execute(list);
+
+            // Assert
+            Assert.AreEqual(expectedNodeData, actual.Data);
+        }
+
+        [TestMethod]
+        public void HeadNodeOnly_ReturnsHeadNode()
+        {
+            // Arrange
+            var expectedNodeData = "A";
+            var list = new LinkedList<string>();
+            list.InsertFirst(expectedNodeData);
+            var sut = new Midpoint<string>();
+
+            // Act
+            var actual = sut.Execute(list);
+
+            // Assert
+            Assert.AreEqual(expectedNodeData, actual.Data);
+        }
+
+        [TestMethod]
+        public void NoHeadNode_ReturnsNull()
+        {
+            // Arrange
+            var sut = new Midpoint<string>();
+            var list = new LinkedList<string>();
+
+            // Act
+            var actual = sut.Execute(list);
+
+            // Assert
+            Assert.IsNull(actual);
+        }
+    }
+}
